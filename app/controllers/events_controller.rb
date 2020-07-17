d70f6488fa222792e98c81039f71553eef51c0f8
mikethreels <michaelthreels@gmail.com>
2020-07-17 23:22:32 +0200

    added the create and new method including view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,9 @@
 class EventsController < ApplicationController
 
+    def new
+        @event = Event.new
+    end
+
     def show
         @event = Event.find(params[:id])
     end
@@ -7,4 +11,26 @@ class EventsController < ApplicationController
     def index
         @events = Event.all
     end
+
+    def create
+        @event = Event.new(event_params)
+        @event.creator_id = current_user.id
+        respond_to do |format|
+          if @event.save
+            format.html { redirect_to events_path, notice: 'Event was successfully created.' }
+            # format.json { render :show, status: :created, location: @user }
+          else
+            format.html { render :new }
+            format.json { render json: @event.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+
+    private
+
+    def event_params
+        params.require(:event).permit(:name, :location, :date, :description)
+    end
+
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
-  resources :events, only: [:show, :index]
+  resources :events, only: [:show, :index, :create, :new]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
