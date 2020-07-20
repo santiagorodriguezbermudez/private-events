fc4d2672d54999dec670573c7bcce3b58d4c8618
mikethreels <michaelthreels@gmail.com>
2020-07-20 22:57:39 +0200

    added list of events to user show

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
 
-    has_many :UserEvent, foreign_key: :attended_event_id
+    has_many :user_events, foreign_key: :attended_event_id
     has_many :attendees, through: :UserEvent, source: :attendee
 end
